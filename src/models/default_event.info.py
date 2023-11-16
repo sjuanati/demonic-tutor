821c1421d6fe1e99686dc31474485608ecad19f1
Sergi Juanati <wint3rmute@gmail.com>
2023-11-16 12:32:02 +0100

    reviewed models info

--- src/models/default_event.info.py
+++ src/models/default_event.info.py
@@ -9,19 +9,24 @@ default_event = {
         "Address of the EVM contract that emits the events to be extracted. "
         "E.g.: 0x6b175474e89094c44da98b954eedeac495271d0f"
     ),
-    "start_block": "Start block number for data extraction. E.g.: 18473342",
-    "end_block": "End block number for data extraction. E.g.: 18473542",
+    "start_block": (
+        "Mandatory: Start block number for data extraction. E.g.: 18473342"
+    ),
+    "end_block": (
+        "Mandatory:   End block number for data extraction." "E.g.: 18473542"
+    ),
     "function_sig": (
-        "Function signature copied from Etherscan or the smart contract code. "
+        "Mandatory: Function signature copied from Etherscan or the smart contract code. "
         "E.g.: Transfer (index_topic_1 address src, index_topic_2 address dst, uint256 wad)"
     ),
     "filters": (
-        "Criteria used to filter events based on indexed parameters. "
+        "Mandatory: Criteria used to filter events based on indexed parameters. "
         "Must contain ALL indexed arguments. If no indexed arguments in signature, use {}. "
-        "E.g.: { 'src': null, 'dst': null }"
+        "Use `null` value when filtering is not needed. ",
+        "E.g.: { 'src': null, 'dst': null }",
     ),
     "decimals": (
-        "Used to format integer values into their decimal representations. "
+        "Optional: Used to format integer values into their decimal representations. "
         "Must contain ALL arguments. Only fill in those that are uint or int. "
         "E.g.: { 'src': null, 'dst': null, 'wad': 18 }"
     ),
